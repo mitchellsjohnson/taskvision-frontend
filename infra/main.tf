9579d712182397eaa7b2b1126e053a9da6bd67ce
Mitchell S. Johnson <mitchellsjohnson@Mac-G957Y0QVQD.local>
2025-05-11 19:33:22 -0400

    feat: update Terraform configuration

--- infra/main.tf
+++ infra/main.tf
@@ -17,6 +17,15 @@ resource "aws_s3_bucket" "frontend" {
   force_destroy = true
 }
 
+resource "aws_s3_bucket_public_access_block" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_website_configuration" "frontend" {
   bucket = aws_s3_bucket.frontend.id
 
