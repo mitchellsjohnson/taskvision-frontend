c32cfe9880031eb8981ad96ec87988b4b11fcd54
Mitchell S. Johnson <mitchellsjohnson@Mac-G957Y0QVQD.local>
2025-05-20 12:26:59 -0400

    feat: use existing ACM certificate if available

--- infra/modules/acm/main.tf
+++ infra/modules/acm/main.tf
@@ -1,6 +1,14 @@
 ### TaskVision ACM Module (START)
 
+data "aws_acm_certificate" "existing" {
+  domain      = var.domain_name
+  statuses    = ["ISSUED"]
+  most_recent = true
+}
+
 resource "aws_acm_certificate" "cert" {
+  count = data.aws_acm_certificate.existing.arn == null ? 1 : 0
+
   domain_name               = var.domain_name
   validation_method         = "DNS"
   subject_alternative_names = var.san_list
@@ -19,23 +27,32 @@ resource "aws_acm_certificate" "cert" {
   }
 }
 
-data "aws_route53_record" "cert_validation" {
+locals {
+  certificate_arn = data.aws_acm_certificate.existing.arn != null ? data.aws_acm_certificate.existing.arn : aws_acm_certificate.cert[0].arn
+}
+
+resource "aws_route53_record" "cert_validation" {
   for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+    for dvo in (data.aws_acm_certificate.existing.arn == null ? aws_acm_certificate.cert[0].domain_validation_options : data.aws_acm_certificate.existing.domain_validation_options) : dvo.domain_name => {
       name   = dvo.resource_record_name
       type   = dvo.resource_record_type
       record = dvo.resource_record_value
     }
   }
 
-  name    = each.value.name
-  type    = each.value.type
-  zone_id = var.zone_id
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = var.zone_id
 }
 
 resource "aws_acm_certificate_validation" "cert_validation" {
-  certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [for record in data.aws_route53_record.cert_validation : record.fqdn]
+  count = data.aws_acm_certificate.existing.arn == null ? 1 : 0
+
+  certificate_arn         = local.certificate_arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
 
 ### TaskVision ACM Module (END)
