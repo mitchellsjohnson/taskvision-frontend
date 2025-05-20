0d00b8d4508f51bd2c53d46a07075fc14d28d2e2
Mitchell S. Johnson <mitchellsjohnson@Mac-G957Y0QVQD.local>
2025-05-20 11:47:33 -0400

    refactor: use static bucket names and update workflow

--- infra/modules/acm/main.tf
+++ infra/modules/acm/main.tf
@@ -19,39 +19,23 @@ resource "aws_acm_certificate" "cert" {
   }
 }
 
-locals {
-  validation_records = {
+data "aws_route53_record" "cert_validation" {
+  for_each = {
     for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
-      record = dvo.resource_record_value
       type   = dvo.resource_record_type
+      record = dvo.resource_record_value
     }
   }
-}
 
-resource "aws_route53_record" "cert_validation" {
-  for_each = local.validation_records
-
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = var.zone_id
-
-  lifecycle {
-    ignore_changes = [
-      name,
-      records,
-      ttl,
-      type
-    ]
-  }
+  name    = each.value.name
+  type    = each.value.type
+  zone_id = var.zone_id
 }
 
 resource "aws_acm_certificate_validation" "cert_validation" {
   certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+  validation_record_fqdns = [for record in data.aws_route53_record.cert_validation : record.fqdn]
 }
 
 ### TaskVision ACM Module (END)
