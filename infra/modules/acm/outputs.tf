6b6e65c59f6c47a316d8f2ff8ca37e93330bc2d5
Mitchell S. Johnson <mitchellsjohnson@Mac-G957Y0QVQD.local>
2025-05-20 20:59:32 -0400

    Force dev build

--- infra/modules/acm/outputs.tf
+++ infra/modules/acm/outputs.tf
@@ -1,9 +1,19 @@
-output "certificate_arn" {
-  description = "ARN of the ACM certificate (either created or reused)"
-  value = length(data.aws_acm_certificate.existing) > 0 ? data.aws_acm_certificate.existing[0].arn : aws_acm_certificate.cert[0].arn
+output "s3_bucket_name" {
+  description = "Name of the S3 bucket"
+  value       = aws_s3_bucket.frontend[0].bucket
 }
 
-output "validation_record_fqdns" {
-  description = "The DNS records to add to validate the certificate"
-  value = length(data.aws_acm_certificate.existing) == 0 ? [for record in aws_route53_record.cert_validation : record.fqdn] : []
+output "cloudfront_distribution_id" {
+  description = "ID of the CloudFront distribution"
+  value       = var.cloudfront_distribution_id != "" ? var.cloudfront_distribution_id : aws_cloudfront_distribution.frontend[0].id
+}
+
+output "cloudfront_domain_name" {
+  description = "Domain name of the CloudFront distribution"
+  value       = var.cloudfront_distribution_id != "" ? "" : aws_cloudfront_distribution.frontend[0].domain_name
+}
+
+output "certificate_arn" {
+  description = "ARN of the ACM certificate"
+  value       = module.acm.certificate_arn
 }
