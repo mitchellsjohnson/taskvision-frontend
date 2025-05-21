0186899b313f2c6a63b5141e07e612374b9c641e
Mitchell S. Johnson <mitchellsjohnson@Mac-G957Y0QVQD.local>
2025-05-20 21:03:56 -0400

    Force frontend build

--- infra/modules/acm/outputs.tf
+++ infra/modules/acm/outputs.tf
@@ -1,19 +1,4 @@
-output "s3_bucket_name" {
-  description = "Name of the S3 bucket"
-  value       = aws_s3_bucket.frontend[0].bucket
-}
-
-output "cloudfront_distribution_id" {
-  description = "ID of the CloudFront distribution"
-  value       = var.cloudfront_distribution_id != "" ? var.cloudfront_distribution_id : aws_cloudfront_distribution.frontend[0].id
-}
-
-output "cloudfront_domain_name" {
-  description = "Domain name of the CloudFront distribution"
-  value       = var.cloudfront_distribution_id != "" ? "" : aws_cloudfront_distribution.frontend[0].domain_name
-}
-
 output "certificate_arn" {
   description = "ARN of the ACM certificate"
-  value       = module.acm.certificate_arn
+  value       = local.certificate_arn
 }
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,13 +1,3 @@
-output "certificate_arn" {
-  description = "ARN of the ACM certificate"
-  value       = module.acm.certificate_arn
-}
-
-output "validation_record_fqdns" {
-  description = "DNS records needed to validate ACM certificate (if any)"
-  value       = module.acm.validation_record_fqdns
-}
-
 output "s3_bucket_name" {
   description = "Name of the S3 bucket"
   value       = local.bucket_id
@@ -15,15 +5,16 @@ output "s3_bucket_name" {
 
 output "cloudfront_distribution_id" {
   description = "ID of the CloudFront distribution"
-  value       = var.cloudfront_distribution_id
+  value       = var.cloudfront_distribution_id != "" ? var.cloudfront_distribution_id : aws_cloudfront_distribution.frontend[0].id
 }
 
 output "cloudfront_domain_name" {
   description = "Domain name of the CloudFront distribution"
-  value       = local.cloudfront_domain_name
+  value       = var.cloudfront_distribution_id != "" ? "" : aws_cloudfront_distribution.frontend[0].domain_name
 }
 
-output "route53_record_name" {
-  description = "DNS record created for the frontend domain"
-  value       = aws_route53_record.frontend_alias.name
+output "certificate_arn" {
+  description = "ARN of the ACM certificate"
+  value       = module.acm.certificate_arn
 }
+
